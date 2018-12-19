c8fd0fcb378f7109762a96a0091bc2968fa68fc2
Michael Peterson <WebDev@ctoms.ca>
2018-12-19 13:37:20 -0700

    added some fake products

---
+++ products/data.js
@@ -0,0 +1,54 @@
+const products = [
+	{
+		id:1,
+		name:"test product",
+		images:["link1","link2"],
+		quantity:5,
+		lastModified: "26-jun-18",
+		price:5.00,
+		class:"class 1",
+		description: " a description"
+	},
+	{
+		id:2,
+		name:"test product 2",
+		images:["link1","link2"],
+		quantity:7,
+		lastModified: "26-jun-18",
+		price:7.00,
+		class:"class 1",
+		description: " a description"
+	},
+	{
+		id:3,
+		name:"test product 3",
+		images:["link1","link2"],
+		quantity:5,
+		lastModified: "26-jun-18",
+		price:5.00,
+		class:"class 1",
+		description: " a description"
+	},
+	{
+		id:4,
+		name: "CTOMS RASH GUARD \"EX COELIS\"",
+		images:["link1","link2"],
+		quantity:8,
+		lastModified: "26-jun-18",
+		price:15.00,
+		class:"class 2",
+		description: "ctoms rash guard"
+	},
+	{
+		id:5,
+		name:"M2-BELT™",
+		images:["link1","link2"],
+		quantity:23,
+		lastModified: "26-jun-18",
+		price:10.00,
+		class:"class 2",
+		description: " a description"
+	},
+];
+
+module.exports = {products}
